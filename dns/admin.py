3a686b7bb33cda12922fab3de0cbc6e1f883e6fb
Wilson Felipe <wfelipe@gmail.com>
2010-04-05 00:00:22 -0300

    fine admin editing

--- dns/admin.py
+++ dns/admin.py
@@ -1,8 +1,10 @@
 from cob.dns.models import *
 from django.contrib import admin
 
-admin.site.register(Domain)
-admin.site.register(Serial)
+class DomainAdmin(admin.ModelAdmin):
+	list_display = ['name', 'current_serial']
 class RecordAdmin(admin.ModelAdmin):
 	list_display = ['name', 'domain']
+admin.site.register(Domain, DomainAdmin)
+admin.site.register(Serial)
 admin.site.register(Record, RecordAdmin)
