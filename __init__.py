a67c711dc508f54d92f8335fe21b848bf578a707
Wilson Felipe <wfelipe@gmail.com>
2010-04-26 18:34:07 -0300

    added a short description of what this project will become

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__about__ = """
+This project is meant to be an administration for sysadmins, where they will be
+able to automate the following areas:
+- dns
+- configuration files
+- inventory
+- etc
+"""
