6284db7e1fca8a39fb654f063e584c6f188d8683
Wilson Felipe <wfelipe@gmail.com>
2010-05-17 22:25:19 -0300

    indent and index

--- dns/urls.py
+++ dns/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^domains/$', 'cob.dns.views.domain_list'),
-    (r'^domains/(?P<domain_id>\d+)/$', 'cob.dns.views.domain_detail'),
-    (r'^domains/compare/?$', 'cob.dns.views.domain_compare'),
-    (r'^domains/new/?$', 'cob.dns.views.domain_new'),
+	(r'^$', 'cob.dns.views.domain_list'),
+	(r'^domains/$', 'cob.dns.views.domain_list'),
+	(r'^domains/(?P<domain_id>\d+)/$', 'cob.dns.views.domain_detail'),
+	(r'^domains/compare/?$', 'cob.dns.views.domain_compare'),
+	(r'^domains/new/?$', 'cob.dns.views.domain_new'),
 )
