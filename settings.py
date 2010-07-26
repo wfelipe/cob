8c70b16ad6c98a0776a8ac0e8856a8b761a8d23c
Wilson Felipe <wfelipe@gmail.com>
2010-07-26 00:45:07 -0300

    ported the database settings to new django version

--- settings.py
+++ settings.py
@@ -9,16 +9,20 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-
-DATABASE_ENGINE = 'sqlite3'      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'cob.data'          # Or path to database file if using sqlite3.
-DATABASE_USER = ''      # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''    # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
-if DATABASE_ENGINE == "mysql":
-	DATABASE_OPTIONS = {"init_command": "SET storage_engine=INNODB"}
+DATABASES = {
+	'default': {
+		'ENGINE': 'mysql',
+		'NAME': 'cob',
+		'USER': 'cob',
+		'PASSWORD': 'cob',
+		'HOST': '',
+		'PORT': '',
+		'OPTIONS': {
+			"autocommit": True,
+			"init_command": "SET storage_engine=INNODB",
+		},
+	}
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
