16c8f96ab598ec91c4ce6658f3996119c327f9ee
Wilson Felipe <wfelipe@gmail.com>
2010-08-25 17:13:15 -0300

    added created and updated field, registering when the items were created

--- cmdb/models.py
+++ cmdb/models.py
@@ -1,15 +1,18 @@
 from django.db import models
+import datetime
 """
 configuration items
 """
 class Manufacturer(models.Model):
 	name = models.CharField(max_length=255, null=False)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	def __unicode__(self):
 		return str(self.name)
 
 class Architecture(models.Model):
 	name = models.CharField(max_length=255, null=False)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	def __unicode__(self):
 		return str(self.name)
@@ -17,6 +20,7 @@ class Architecture(models.Model):
 class ServerType(models.Model):
 	name = models.CharField(max_length=255, null=False)
 	manufacturer = models.ForeignKey('Manufacturer', null=True)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	def __unicode__(self):
 		return str(self.name)
@@ -26,6 +30,8 @@ class Server(models.Model):
 	memory = models.FloatField(null=False)
 	serial = models.CharField(max_length=255, null=True, default=None)
 	virtual = models.BooleanField(null=False)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
+	updated = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	operating_system = models.ForeignKey('OperatingSystem', null=False)
 	server_type = models.ForeignKey('ServerType', null=False)
@@ -38,6 +44,7 @@ class OperatingSystem(models.Model):
 	name = models.CharField(max_length=255, null=False)
 	version = models.CharField(max_length=16, null=False)
 	architecture = models.ForeignKey('Architecture', null=False)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	class Meta:
 		unique_together = [("name", "version", "architecture")]
@@ -48,6 +55,8 @@ class OperatingSystem(models.Model):
 class NetworkInterface(models.Model):
 	name = models.CharField(max_length=8, null=False)
 	macaddress = models.CharField(max_length=18)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
+	updated = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	ipaddress = models.ForeignKey('InternetAddress', unique=True)
 	server = models.ForeignKey('Server', null=False)
@@ -57,6 +66,7 @@ class NetworkInterface(models.Model):
 
 class InternetAddress(models.Model):
 	address = models.CharField(max_length=255, null=False, unique=True)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	network = models.ForeignKey('Network', null=False)
 
@@ -67,6 +77,7 @@ class Network(models.Model):
 	name = models.CharField(max_length=255, null=False)
 	netmask = models.CharField(max_length=255, null=False)
 	network = models.CharField(max_length=255, null=False)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	def __unicode__(self):
 		return str(self.name)
@@ -77,6 +88,7 @@ Puppet Class, automation
 class PuppetClass(models.Model):
 	name = models.CharField(max_length=255, null=False, unique=True)
 	description = models.CharField(max_length=255)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	def __unicode__(self):
 		return str(self.name)
@@ -87,6 +99,7 @@ System
 class System(models.Model):
 	name = models.CharField(max_length=255, null=False, unique=True)
 	description = models.CharField(max_length=255, null=True)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	puppetclass = models.ManyToManyField(PuppetClass, through='SystemPuppetClass')
 
@@ -96,6 +109,7 @@ class System(models.Model):
 class SystemPuppetClass(models.Model):
 	system = models.ForeignKey(System, null=False)
 	puppetclass = models.ForeignKey(PuppetClass, null=False)
+	created = models.DateTimeField(default=datetime.datetime.now, null=False)
 
 	def __unicode__(self):
 		return(self.system.name)
